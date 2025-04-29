897284eceb4d6261d3e1a2eb327290152efc1a3b
Jandel <jandel456@gmail.com>
2025-04-29 11:55:47 -0400

    Addition of Web Pages File
    The structure of the app is within the Web Pages file, which holds the login, signin, quizzes, and homepage

---
+++ Web Pages/LoginPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Online_Exam_System_Application_2.Web_Pages;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Web Pages/Quiz1.xaml.cs
@@ -0,0 +1,9 @@
+namespace Online_Exam_System_Application_2.Web_Pages;
+
+public partial class Quiz1 : ContentPage
+{
+	public Quiz1()
+	{
+		InitializeComponent();
+	}
+}
